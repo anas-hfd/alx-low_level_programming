f1e6ec58b8d2371dd1db0be71a9e07fdf987473b
anas-hfd <anas1.hfd@gmail.com>
2023-03-17 06:15:53 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,23 +9,23 @@
 int main(void)
 {
 	int n;
-	int lastdigit;
+	int lstdgt;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastdigit = n % 10;
+	lstdgt = n % 10;
 
-	if (lastdigit > 5)
+	if (lstdgt > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lstdgt);
 	}
-	else if (lastdigit == 0)
+	else if (lstdgt == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is 0\n", n, lstdgt);
 	}
-	else if (lastdigit < 6 && lastdigit != 0)
+	else if (lstdgt < 6 && lstdgt != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstdgt);
 	}
 	return (0);
 }
