19ea78ec2aa225824e09c472d3051d87b844500e
anas-hfd <anas1.hfd@gmail.com>
2023-03-17 16:00:41 -0700

    OMG git add .git add . so many combs

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i; j <= 99; j++)
+		for (j = 0; j < 100; j++)
 		{
-			if (i != j)
+			if (i < j)
 			{
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar(j / 10 + '0');
-				putchar(j % 10 + '0');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			if (i != 99 && j != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+
 		}
 	}
 	putchar('\n');
