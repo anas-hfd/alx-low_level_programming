0edabe925adcddae8882fc0f25b1a215de568252
anas-hfd <anas1.hfd@gmail.com>
2023-03-21 14:39:37 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,8 @@ void jack_bauer(void)
 	{
 	for (b = 0; b <= 9; b++)
 	{
-	if ((a <= 2 && b <= 3) || (a <= 1 && b <=9))
-	{	
+	if ((a <= 2 && b <= 3) || (a <= 1 && b <= 9))
+	{
 	for (c = 0; c <= 5; c++)
 	{
 	for (d = 0; d <= 9; d++)
