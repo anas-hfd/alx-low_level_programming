91a919eded5424aae3a8bc9720900d5374383d3d
anas-hfd <anas1.hfd@gmail.com>
2023-03-21 08:18:53 -0700

    10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printalphabet 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
