d1efedb0cd3858f90a40c01eccbea8bf62b73458
anas-hfd <anas1.hfd@gmail.com>
2023-03-22 07:56:45 -0700

    checking the greatest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > c)
 	{
 		largest = b;
 	}
-	else if (c > b && c > a)
+	else
 	{
 		largest = c;
 	}
