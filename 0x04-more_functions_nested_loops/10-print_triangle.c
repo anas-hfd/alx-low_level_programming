b15ed7181c676ce3997eb7512a802da3968650b8
anas-hfd <anas1.hfd@gmail.com>
2023-03-23 16:11:34 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - fun that prints a triangle
+ * @size: the size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+	int c;
+
+	if (size <= 0)
+        {
+        _putchar('\n');
+        }
+	else 
+	{
+	for (a = 0; a < size ; a++)
+	{
+	for (b = size - a; b > 1 ; b--)
+	{
+	_putchar(32);
+	}
+	for (c = 0; c <= a; c++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
