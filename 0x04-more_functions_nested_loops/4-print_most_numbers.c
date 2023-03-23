ffadd19f0d9155c719c9faf28b86ec2c3bb8a1c4
anas-hfd <anas1.hfd@gmail.com>
2023-03-23 05:36:14 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - do not print 2 and 4
+ *
+ * Return: 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+	if (a != 2 && a != 4)
+	{
+		_putchar(a + '0');
+		a++;
+	}
+	else
+		a++;
+	}
+	_putchar('\n');
+}
