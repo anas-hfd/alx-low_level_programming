193444eed786f1b302a8dcb56a2e3c8fb949da62
anas-hfd <anas1.hfd@gmail.com>
2023-03-30 07:49:37 -0700

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,17 +9,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < n && dest[i] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 	while (i < n)
 	{
-		dest[i] = '\0';
-		i++;
+		dest[i++] = '\0';
 	}
 	return (dest);
 
