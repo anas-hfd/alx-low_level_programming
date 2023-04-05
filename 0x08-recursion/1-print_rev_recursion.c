fec143ed7076b0126d456dc65f0c92b7a6fc9412
anas-hfd <anas1.hfd@gmail.com>
2023-04-05 07:27:30 -0700

    rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints reversed string
+ * @s: string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
