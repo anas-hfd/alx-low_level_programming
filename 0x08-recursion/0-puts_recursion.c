f80b75a12bd250daa8ef08653567a45615f23858
anas-hfd <anas1.hfd@gmail.com>
2023-04-05 06:13:07 -0700

    with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		_putchar(*s);
-		s++;
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
 }
