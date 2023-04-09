347942cdc7436375f4558ed5b8abe5167b4379f2
anas-hfd <anas1.hfd@gmail.com>
2023-04-09 08:44:30 -0700

    first argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: int
+ */
+int main(int unused_variable __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
