47ea6d3c7266393988f236d474301bc87b262233
anas-hfd <anas1.hfd@gmail.com>
2023-04-11 07:30:18 -0700

    alloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: string
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+	int len;
+	char *dup;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+	len = strlen(str);
+	dup = malloc(len * sizeof(char));
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		strcpy(dup, str);
+	}
+	}
+	return (dup);
+}
