8fc29a5d56b77f61272a6572844654a61067383c
anas-hfd <anas1.hfd@gmail.com>
2023-04-12 05:15:21 -0700

    free it the right way

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,21 +18,22 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	grid = malloc(height * sizeof(int));
+	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = calloc(width, sizeof(int));
+		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
 			{
-				free(grid);
+				free(grid[i]);
 				return (NULL);
 			}
+			free(grid);
 		}
 	}
 	return (grid);
