e245738228d081facb8ca71c8cd075758f7e9761
anas-hfd <anas1.hfd@gmail.com>
2023-04-12 07:08:17 -0700

    allocating the right amount of memory

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,7 +25,7 @@ char *argstostr(int ac, char **av)
 		len = strlen(av[i]);
 		total = total + len + 1;
 	}
-	conc = malloc(sizeof(char) * total);
+	conc = (char *)malloc(sizeof(char) * total);
 	if (conc == NULL)
 	{
 		return (NULL);
