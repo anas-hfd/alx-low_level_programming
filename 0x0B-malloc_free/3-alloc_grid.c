4bc00129b3b07f341408815b0e28ff9a051fcde0
anas-hfd <anas1.hfd@gmail.com>
2023-04-12 05:20:09 -0700

    free memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,9 +28,9 @@ int **alloc_grid(int width, int height)
 		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 0; j < width; j++)
 			{
-				free(grid[i]);
+				free(grid[j]);
 				return (NULL);
 			}
 			free(grid);
