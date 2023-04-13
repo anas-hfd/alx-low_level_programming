814b961e6dbf974ffda3729c9206a4d83b19bb6e
anas-hfd <anas1.hfd@gmail.com>
2023-04-13 07:35:42 -0700

    new correction

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,50 +1,74 @@
-#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
+
 /**
- * strtow - splits a tring into separated words
+ * count_words - helper counts the number of words
  * @str: string
- * Return: char
+ * Return: number of words
+ */
+int count_words(char *str)
+{
+	int word, char_index, count;
+
+	word = 0;
+	count = 0;
+
+	for (char_index = 0; str[char_index] != '\0'; char_index++)
+	{
+		if (str[char_index] == ' ')
+			word = 0;
+		else if (word == 0)
+		{
+			word = 1;
+			count++;
+		}
+	}
+
+	return (count);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: ptr to arr(Success) or NULL (Error)
  */
 char **strtow(char *str)
 {
-	int a, b = 0, c, i = 0, len = strlen(str), wlen = 0;
-	char **words;
+	char **matrix, *tmp;
+	int i, index = 0, str_len = 0, num_words, char_count = 0;
+	int word_start, word_end;
 
-	for (a = 0; a < len; a++)
-	{
-		if (str[a] != ' ' && (a == 0 || str[a - 1] == ' '))
-			i++;
-	}
-	words = (char **)malloc(sizeof(char *) * (i + 1));
-	if (words == NULL)
-	{
+	while (*(str + str_len))
+		str_len++;
+	num_words = count_words(str);
+	if (num_words == 0)
 		return (NULL);
-	}
-	for (a = 0; a < len; a++)
+	matrix = (char **) malloc(sizeof(char *) * (num_words + 1));
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i <= str_len; i++)
 	{
-		if (str[a] != ' ' && (a == 0 || str[a - 1] == ' '))
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			wlen = 0;
-			for (c = a; c < len && str[c] != ' '; c++)
+			if (char_count)
 			{
-				wlen++;
+				word_end = i;
+				tmp = (char *) malloc(sizeof(char) * (char_count + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (word_start < word_end)
+					*tmp++ = str[word_start++];
+				*tmp = '\0';
+				matrix[index] = tmp - char_count;
+				index++;
+				char_count = 0;
 			}
-			words[b] = (char *)malloc(sizeof(char) * (wlen + 1));
-			if (words[b] == NULL)
-			{
-				for (a = 0; a < b; a++)
-				{
-					free(words[a]);
-				}
-				free(words);
-				return (NULL);
-			}
-			strncpy(words[b], &str[a], wlen);
-			words[b][wlen] = '\0';
-			b++;
 		}
+		else if (char_count++ == 0)
+			word_start = i;
 	}
-	words[i] = NULL;
-	return (words);
+	matrix[index] = NULL;
+	return (matrix);
 }
+
