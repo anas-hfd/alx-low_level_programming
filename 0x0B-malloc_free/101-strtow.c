d0a3ea717dd4c620f8441e81c8f6349cc65f03e7
anas-hfd <anas1.hfd@gmail.com>
2023-04-12 18:37:20 -0700

    fun

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * strtow - splits a tring into separated words
+ * @str: string
+ * Return: char
+ */
+char **strtow(char *str)
+{
+	int a, b = 0, c, i = 0, wlen = 0, len = strlen(str);
+	char **words;
+
+	for (a = 0; a < len; a++)
+	{
+		if (str[a] != ' ' && (a == 0 || str[a - 1] == ' '))
+			i++;
+	}
+	words = (char **)malloc(sizeof(char *) * (i + 1));
+	if (words == NULL)
+		return (NULL);
+	for (a = 0; a < len; a++)
+	{
+		if (str[a] != ' ' && (a == 0 || str[a - 1] == ' '))
+		{
+			for (c = a; c < len && str[c] != ' '; c++)
+			{
+				wlen++;
+			}
+			words[b] = (char *)malloc(sizeof(char) * (wlen + 1));
+			if (words[b] == NULL)
+			{
+				for (a = 0; a < b; a++)
+				{
+					free(words[a]);
+				}
+				free(words);
+				return (NULL);
+			}
+			strncpy(words[b], &str[i], wlen);
+			words[b][wlen] = '\0';
+			b++;
+		}
+	}
+	words[i] = NULL;
+	return (words);
+}
