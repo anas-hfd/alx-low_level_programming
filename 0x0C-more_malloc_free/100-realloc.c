8d8028d692eedbcf17b2f50e19b651098a120550
anas-hfd <anas1.hfd@gmail.com>
2023-04-14 19:02:34 -0700

    more free memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,11 +17,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == 0 && ptr != NULL)
 	{
+		free(ptr);
 		return (NULL);
 	}
 	if (ptr == NULL)
 	{
 		ptr2 = malloc(new_size);
+		if (ptr2 == NULL)
+		{
+			return (NULL);
+		}
+		free(ptr);
 		return (ptr2);
 	}
 	if (new_size == old_size)
