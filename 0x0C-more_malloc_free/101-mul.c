4847f241b468b0bd810f785428643786d7d07c0c
anas-hfd <anas1.hfd@gmail.com>
2023-04-15 07:11:25 -0700

    no big numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	char *num1, *num2;
+	int n1, n2, mul;
+	long unsigned int i;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = argv[1];
+	num2 = argv[2];
+
+	for (i = 0; i < strlen(num1); i++)
+	{
+		if (!isdigit(num1[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	for (i = 0; i < strlen(num2); i++)
+	{
+		if (!isdigit(num2[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	n1 = atoi(num1);
+	n2 = atoi(num2);
+	mul = n1 * n2;
+	printf("%d\n", mul);
+	return (0);
+}
