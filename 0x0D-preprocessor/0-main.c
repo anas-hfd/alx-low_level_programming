6003575b4813702d641e43684ac05d65c12f9805
anas-hfd <anas1.hfd@gmail.com>
2023-04-17 07:52:18 -0700

    define size

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+#define SIZE 1024
+int main(void)
+{
+    int s;
+
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+new directory to start learning about preprocessor, macros and guard.
