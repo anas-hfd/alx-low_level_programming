3c4e3c9b0fa940c200c574b0911b50157686d34c
anas-hfd <anas1.hfd@gmail.com>
2023-04-17 19:00:22 -0700

    tired of errors

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,14 +11,14 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 /**
  * dog_t - struct dog's typedef
  */
 typedef struct dog dog_t;
 
-voi(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
