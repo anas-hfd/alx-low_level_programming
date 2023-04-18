9144dde1ae5c8f2191348b728d7d5f7c79c28255
anas-hfd <anas1.hfd@gmail.com>
2023-04-17 18:52:59 -0700

    betty is the real ennimy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,15 +11,14 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 /**
  * dog_t - struct dog's typedef
  */
-
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+voi(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
