c76da52e4e7b15b2117dd26c9235877f47b47a15
anas-hfd <anas1.hfd@gmail.com>
2023-04-20 08:11:11 -0700

    numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,13 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(ap, int);
-		if (separator == NULL)
+		printf("%d ", num);
+		if (separator != NULL && i < n - 1)
 		{
-			printf("%d ", num);
-		}
-		else
-		{
-			printf("%d%s ", num, separator);
+			printf("%s ", separator);
 		}
 	}
 	printf("\n");
