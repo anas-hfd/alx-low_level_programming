0b86da961678ec570f08807ab46109ade40815f5
anas-hfd <anas1.hfd@gmail.com>
2023-04-19 18:58:47 -0700

    that i what we call an advanced task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_opcodes(int num_bytes);
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+
+int main(int argc, char **argv)
+{
+	int num_bytes = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	else
+	{
+		print_opcodes(num_bytes);
+		return (0);
+	}
+}
+
+/**
+ * print_opcodes - prints opcodes
+ * @num_bytes: number of bytes
+ * Return: int
+ */
+
+void print_opcodes(int num_bytes)
+{
+	int i;
+	unsigned char *ptr = (unsigned char *)print_opcodes;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x ", ptr[i]);
+	}
+	printf("\n");
+}
