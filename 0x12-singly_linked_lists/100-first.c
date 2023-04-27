1ad6db8756b108f04631186190a7c15b3469339e
anas-hfd <anas1.hfd@gmail.com>
2023-04-27 16:41:42 -0700

    attribute constractor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - simple print
+ *
+ * Return: void
+ */
+
+void __attribute__ ((constructor)) first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return;
+}
