c9bce6e58d8aedeca155500b7c78ee6c74b2d9ee
anas-hfd <anas1.hfd@gmail.com>
2023-05-02 14:34:14 -0700

    more free mem

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a list
+ * @head: ptr to ptr of athe head
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node = *head, *helper;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+	else
+	{
+		while (node != NULL)
+		{
+			helper = node;
+			node = node->next;
+			free(helper);
+		}
+	}
+	*head = NULL;
+}
