13f0c216803e1ecf3c1088f37c12e4f2aa3f181b
anas-hfd <anas1.hfd@gmail.com>
2023-05-02 16:00:20 -0700

    the sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_listint - the summ of data
+ * @head: ptr to head
+ * Return: the sum of data
+ */
+
+int sum_listint(listint_t *head)
+{
+	int n, i ,s = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; head != NULL;i++)
+		{
+			n = head->n;
+			s = s + n;
+			head = head->next;
+		}
+	}
+	return (s);
+}
