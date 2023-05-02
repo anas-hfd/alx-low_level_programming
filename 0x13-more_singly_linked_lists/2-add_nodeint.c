081a28d86627d408c17d6e9bc35ce633b9f0e927
anas-hfd <anas1.hfd@gmail.com>
2023-05-02 13:02:13 -0700

    add node to the head

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+	}
+	return (new);
+}
