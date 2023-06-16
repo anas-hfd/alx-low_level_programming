3746eb5bd80d286b8281bdc6ac52db9cf908a410
anas-hfd <anas1.hfd@gmail.com>
2023-06-16 06:21:48 -0700

    dll

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - prints a doubly linked list
+ *@h: head
+ *Return: dll
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i;
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+ it is C again : doubly linked lists
