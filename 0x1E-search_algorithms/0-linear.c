31e11f0dd2aa40cb6700ec80c90f7ab960d274bd
anas-hfd <anas1.hfd@gmail.com>
2023-10-10 18:09:17 +0100

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 	{
 		return (-1);
 	}
@@ -21,7 +21,6 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			printf("Found %d at index: %lu\n", value, i);
 			return (i);
 		}
 	}
